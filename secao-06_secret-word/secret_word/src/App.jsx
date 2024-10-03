f09ff16ef5d2e0b8d44eea2cc3717b867eede9e8
Jopereira2005 <jopereiravieira98@gmail.com>
2024-10-03 07:54:02 -0300

    .

--- secao-06_secret-word/secret_word/src/App.jsx
+++ secao-06_secret-word/secret_word/src/App.jsx
@@ -22,8 +22,44 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState();
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  }
+
   // starts  the game
   const startGame = () => {
+    // pick word and pick category
+    const { category, word } = pickWordAndCategory();
+
+    // creat an array of letters
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(category, word);
+    console.log(wordLetters);
+
+
+    // fill states
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters);
+
     setGameStage(stages[1].name);
   }
 
@@ -40,7 +76,18 @@ function App() {
   return (
     <div className='App'>
       {gameStage == "start" && <StartScreen startGame={ startGame }/>}
-      {gameStage == "game" && <Game verifyLetter={ verifyLetter }/>}
+      {gameStage == "game" && 
+        <Game 
+          verifyLetter = { verifyLetter }
+          pickedWord = { pickedWord }
+          pickedCategory = { pickedCategory }
+          letters = { letters }
+          guessedLetters = { guessedLetters }
+          wrongLetters = { wrongLetters }
+          guesses = { guesses }
+          score = { score }
+        />
+      }
       {gameStage == "end" && <GameOver retry={ retry }/>}
     </div>
   )
--- secao-06_secret-word/secret_word/src/components/Game.jsx
+++ secao-06_secret-word/secret_word/src/components/Game.jsx
@@ -1,12 +1,51 @@
 import './Game.css'
 
-const Game = ({ verifyLetter }) => {
+const Game = ({ 
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score
+}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={ verifyLetter }>Finalizar o Jogo</button>
-    </div>
+    <div className="game">
+      <p className="points">
+          <span>Pontuação: { score }</span>
+      </p>
+      <h1>Adivinhe a palavra</h1>
+      <h3 className="tip">
+        Dica sovre a palavra: <span>{ pickedCategory }</span>
+      </h3>
+      <p>Você ainda tem { guesses } tentativas</p>
+      <div className="wordContainer">
+        {letters.map((letter, i) => 
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">{letter}</span>
+          ) : (
+            <span className="blankSquare"></span>
+          )
+        )}
+          <span className="letter">A</span> 
+          <span className="blankSquare"></span>   
 
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button onClick={ verifyLetter }>Jogar</button>
+        </form>
+      </div>
+      <div className="worgLettersContainer">
+        <p>Letra já utilizadas:</p>
+        <span>a</span>
+        <span>b</span>
+      </div>
+      <h1>Game</h1>
+    </div>
   )
 }
 
